54df8b09f2c577de2834ca1a8c41ee22d0bdc152
Zephion <45722962+Zephion@users.noreply.github.com>
2019-01-26 17:39:51 +0100

    Add files via upload

--- crawler.py
+++ crawler.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from synonymes import *
-
+import sys
 
 class Crawler:
 
@@ -9,20 +9,23 @@ class Crawler:
 	def web(self,page,WebUrl,mot):
 	    if(page>0):
 	        url = WebUrl
-	        print("Mot: "+mot)
+	        motAtrouver = mot
+	        print("Mot: "+motAtrouver)
 	        code = requests.get("http://www.dictionnaire-synonymes.com/synonyme.php?mot="+mot+"&OK=OK")
 	        plain = code.text
 	        s = BeautifulSoup(plain, "html.parser")
 	        syn = synonymes()
 	        definition = syn.RechercheSyn(mot)
 	        print("\nLa définition trouvé est: " + definition)
+         	   
 	        for link in s.findAll('a', {'class':'lien3'}):
 	            #mot = link.find('a', {'class':'lien3'})
 	            mot = str(link.text)
 	            if mot!="[informal]" and mot:
-	            	print("Synonymes: "+ str(link.text))
-	           
-
+	                print("Synonymes: "+ str(link.text))
+	                syn.InsertionSyn(motAtrouver, str(link.text))
+	        
+	    #return definition, mot
 
 
 
