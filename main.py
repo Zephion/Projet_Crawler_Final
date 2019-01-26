c936bba5325cc353832ba53a85b1e1da732fece7
Zephion <45722962+Zephion@users.noreply.github.com>
2019-01-26 17:40:33 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,30 +1,22 @@
 #from Client import Client
 from crawler import Crawler
 from database import Base_donnee
-from verifi import *
 
 
 TCP_IP = "node.nicopr.fr"
 TCP_PORT = 55555
 BUFFER_SIZE = 2048
+i = 1
+X = Base_donnee()
 
+while i == 1:
+	MotAchercher = input("\n>> Entrer un mot a chercher: ")
 
-X = Verifi()
-#client = Client(TCP_IP,TCP_PORT,BUFFER_SIZE)
-#data = client.connectToServer()
+	Y=X.verification(MotAchercher)
 
-MotAchercher = input(">> Entrer un mot a chercher: ")
+	if(Y == None):
+		Crawler().web(1,"https://fr.wiktionary.org/wiki/", MotAchercher)
+	else:
+		print("Le mot: ", MotAchercher," est déjà presente dans la base de donnée.")
 
-Y=X.verification(MotAchercher)
-
-#[definition, synonyme] = Crawler().web(1,"https://fr.wiktionary.org/wiki/", MotAchercher)
-if(Y == None):
-	Crawler().web(1,"https://fr.wiktionary.org/wiki/", MotAchercher)
-else:
-   print("Le mot: ", MotAchercher," est déjà presente dans la base de donnée.")
-
-#print("\n Partie main:\n")
-#print(definition)
-#print("\n\n")
-#print(synonyme)
 
